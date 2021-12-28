98695670762322fb1993bafe633da6b4df7eaf2f
BeatrixKiddo <80118008+BeatrixKiddo1@users.noreply.github.com>
2021-12-28 14:25:02 +0200

    Changed

--- client/src/app/users/users.component.ts
+++ client/src/app/users/users.component.ts
@@ -5,24 +5,29 @@ import UsersJson from '../../assets/users.json';
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.css']
+  styleUrls: ['./users.component.css'],
 })
 
 export class UsersComponent implements OnInit {
+  object: Object = UsersJson;
   
-  getUsers(special:any):void 
-  {  
-    fetch("../../assets/users.json")
-    .then( data => special.innerText = `All Users:
-    ${JSON.stringify(UsersJson).replace(/,|}|{|\\/g, "\n")}`, 
-    error => console.log(error))
-    .catch(err =>console.log(err));  
-  }
+  getUsers(special:any):void{
+  special.innerText = JSON.stringify(UsersJson).replace(/,|}|{|\\/g, "\n");
+}  
 
-  getDocument():any{    
-    var currentElement = document.querySelector("#special");
-    return currentElement;
-  }
+  // getUsers(special:any):void 
+  // {  
+  //   fetch("../../assets/users.json")
+  //   .then( data => special.innerText = `All Users:
+  //   ${JSON.stringify(UsersJson).replace(/,|}|{|\\/g, "\n")}`, 
+  //   error => console.log(error))
+  //   .catch(err =>console.log(err));  
+  // }
+
+  // getDocument():any{    
+  //   let currentElement = document.querySelector("#special");
+  //   return currentElement;
+  // }
 
 
   constructor() { }
